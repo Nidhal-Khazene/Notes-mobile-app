936a296c7bbdf847d652a650cbc90ab5e53f4f53
nidhal khazene <nidhal.khazene@gmail.com>
2025-07-05 19:39:42 +0100

    add Edit Note View

--- lib/widgets/custom_widgets/custom_list_tile.dart
+++ lib/widgets/custom_widgets/custom_list_tile.dart
@@ -1,51 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class CustomListTile extends StatelessWidget {
   const CustomListTile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 16, bottom: 16, left: 16),
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text(
-              "Flutter tips",
-              style: TextStyle(color: Colors.black, fontSize: 26),
-            ),
-            subtitle: Padding(
-              padding: EdgeInsets.only(top: 16, bottom: 16),
-              child: Text(
-                "Build your career with Nidhal Khazene",
-                style: TextStyle(
-                  color: Colors.black.withValues(alpha: 0.5),
-                  fontSize: 18,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return EditNoteView();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 16, bottom: 16, left: 16),
+        decoration: BoxDecoration(
+          color: const Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text(
+                "Flutter tips",
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
+              subtitle: Padding(
+                padding: EdgeInsets.only(top: 16, bottom: 16),
+                child: Text(
+                  "Build your career with Nidhal Khazene",
+                  style: TextStyle(
+                    color: Colors.black.withValues(alpha: 0.5),
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 28,
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(FontAwesomeIcons.trash, color: Colors.black, size: 28),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(right: 24),
-            child: Text(
-              "July 04,2025",
-              style: TextStyle(
-                color: Colors.black.withValues(alpha: 0.5),
-                fontSize: 16,
+            Padding(
+              padding: EdgeInsets.only(right: 24),
+              child: Text(
+                "July 04,2025",
+                style: TextStyle(
+                  color: Colors.black.withValues(alpha: 0.5),
+                  fontSize: 16,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
